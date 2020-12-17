b1902d38616a1f00c4071502af508bdb24891482
wonghongkeat <hongkeat1991@gmail.com>
2020-12-17 02:25:09 +0000

    edited:
    install cors and able to link with vue

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const MongoUtil = require('./MongoUtil.js')
 const ObjectId = require('mongodb').ObjectId;
+const cors = require('cors')
 
 
 // load in environment variables
@@ -12,6 +13,7 @@ const app = express();
 app.set('view engine','hbs')
 app.use(express.static('public'))
 app.use(express.json());
+app.use (cors())
 
 
 async function main() {
