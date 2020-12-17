ce0af886ec49fc207e58dba80726bc96b4fac8f6
wonghongkeat <hongkeat1991@gmail.com>
2020-12-17 10:01:25 +0000

    edited:

--- index.js
+++ index.js
@@ -10,60 +10,60 @@ require('dotenv').config();
 
 // create the app
 const app = express();
-app.set('view engine','hbs')
+app.set('view engine', 'hbs')
 app.use(express.static('public'))
 app.use(express.json());
-app.use (cors())
+app.use(cors())
 
 
 async function main() {
-    const MONGO_URL=process.env.MONGO_URL;
+    const MONGO_URL = process.env.MONGO_URL;
     await MongoUtil.connect(MONGO_URL, "tgc9_assignment2");
     let db = MongoUtil.getDB();
 
     // to retrieve the database
-    app.get('/', async function(req,res){
+    app.get('/', async function (req, res) {
         let listings = await db.collection('players_score').find().toArray()
         res.send(listings)
-      
+
     })
 
     // to retrieve specific data
-    app.get('/:id', async function(req,res){
+    app.get('/:id', async function (req, res) {
         let score = await db.collection('players_score').findOne({
-            '_id':ObjectId(req.params.id)
+            '_id': ObjectId(req.params.id)
         })
         res.send(score)
     })
 
     // to delete
-    app.delete('/:id', async function(req,res){
+    app.delete('/:id', async function (req, res) {
         await db.collection('players_score').deleteOne({
             _id: ObjectId(req.params.id)
         })
         res.send({
-            'status':'ok'
+            'status': 'ok'
         })
     })
     //to edit
-    app.patch('/:id', async function(req,res){
-        let {name,score} = req.body
+    app.patch('/:id', async function (req, res) {
+        let { name, score } = req.body
         await db.collection('players_score').updateOne({
-            '_id':ObjectId(req.params.id)
+            '_id': ObjectId(req.params.id)
         },
-        {
-            '$set' :{name, score}
-        })
+            {
+                '$set': { name, score }
+            })
         res.send('update done')
     })
 
     // to create
-    app.post('/', async function(req,res){
+    app.post('/', async function (req, res) {
         let {
             name, score
         } = req.body
         await db.collection('players_score').insertOne({
-            name,score
+            name, score
         })
         res.send('new info created')
     })
@@ -76,6 +76,6 @@ async function main() {
 }
 
 main()
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("Express is running")
 })
